ddbdfa980d4389d5c40fb79700a34383dfdd1181
Zachary Williams <kcaz311@gmail.com>
2021-07-28 14:33:12 -0400

    built store and gameSlice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { Joker1 } from "./cards";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
