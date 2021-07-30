eb86b863fea56e6ff5bc82d5eb572dc6805bb2fb
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-30 17:33:24 -0500

    fixed slice import

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import "./App.css";
 import { Joker1 } from "./cards";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Board } from "./Components/Board/Board";
 import Card from "./Components/Card/Card";
 function App() {
   return (
     <Router>
       <div className="App">
-        <main className='flexbox'>
+        <main className="flexbox">
           <br />
-          <Board id='board-1' className='board'>
-            <Card id="card-1" className='card' draggable='true'>
-                  <p>card one</p>
+          <Board id="board-1" className="board">
+            <Card id="card-1" className="card" draggable="true">
+              <p>card one</p>
             </Card>
           </Board>
 
-          <Board id='board-2' className='board'>
-            <Card id="card-2" className='card' draggable='true'>
-                  <p>card two</p>
+          <Board id="board-2" className="board">
+            <Card id="card-2" className="card" draggable="true">
+              <p>card two</p>
             </Card>
           </Board>
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import gameSlice from "./gamesSlice";
+import gameSlice from "./gameSlice";
 import playerSlice from "./playerSlice";
 
 export const store = configureStore({
