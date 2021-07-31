06b363a39c39ab2b696c65116e60fbee745e5642
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-30 19:08:31 -0500

    scaffolded out load scheme, added evergreen-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { Joker1 } from "./Components/Card/CardFaces";
 import { BrowserRouter as Router } from "react-router-dom";
 import Board from "./Components/Board/Board";
 import Card from "./Components/Card/Card";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
+  const handleLoad = () => {
+    // check localStorage for existing game and player ids, use to determine which component to load
+    const storageContents = {
+      playerId: localStorage.getItem("playerId"),
+      gameId: localStorage.getItem("gameId"),
+    };
+
+    if (storageContents.playerId && storageContents.gameId) {
+      // dispatch get game and get player for relevant ids
+      // if game and player exist
+      // render game board
+      // this might be its own function
+    } else {
+      // render game list with about modal on
+    }
+  };
+
   return (
     <Router>
       <div className="App">
